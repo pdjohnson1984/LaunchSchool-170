9bb7d68b88a6cc1b252f11e12fb0a6f873962c19
Phillip Johnson <pdjohnson84@gmail.com>
2016-11-23 13:24:46 -0800

    Working on book reading app

--- Working with Sinatra/book_viewer/book_viewer.rb
+++ Working with Sinatra/book_viewer/book_viewer.rb
@@ -3,5 +3,16 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
   erb :home
+end
+
+get "/chapter/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
 end
