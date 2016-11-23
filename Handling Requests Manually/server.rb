c62a88004a04a1c8cc3ef4ed13da68c85be70757
Phillip Johnson <pdjohnson84@gmail.com>
2016-11-22 21:30:23 -0800

    Finishing server

--- Handling Requests Manually/server.rb
+++ Handling Requests Manually/server.rb
@@ -1,12 +1,54 @@
 require "socket"
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = (params || "").split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
+
 server = TCPServer.new("localhost", 3003)
 loop do
   client = server.accept
 
   request_line = client.gets
+  # next if !request_line || request_line =~ /favicon/
+
   puts request_line
 
-  client.puts request_line
+  http_method, path, params = parse_request(request_line)
+
+  # client.puts request_line
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  # client.puts "<h1>Rolls!</h1>"
+
+  # rolls = params["rolls"].to_i
+  # sides = params["sides"].to_i
+  #
+  # rolls.times do
+  #   roll = rand(sides) +1
+  #   client.puts "<p>", roll, "</p>"
+  # end
+
+  client.puts "<h1>Counter</h2>"
+
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='http://localhost:3003/?number=#{number + 1}'>Add One</a>"
+  client.puts "<br>"
+  client.puts "<a href='http://localhost:3003/?number=#{number - 1}'>Subtract One</a>"
+
+  client.puts "</body>"
+  client.puts "</html>"
   client.close
 end
