84f409c281d940e3e5fbdb149b9bfc825c8a5789
Phillip Johnson <pdjohnson84@gmail.com>
2016-11-23 16:05:26 -0800

    working on projects

--- Working with Sinatra/book_viewer/book_viewer.rb
+++ Working with Sinatra/book_viewer/book_viewer.rb
@@ -2,17 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map { |par| '<p>' + par + '</p>'}.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
-get "/chapter/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapter/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
