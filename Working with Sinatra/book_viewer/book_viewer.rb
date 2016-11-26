ba5929b6f38bb157ee5524e9b086687ec3a2557c
Phillip Johnson <pdjohnson84@gmail.com>
2016-11-25 17:32:28 -0800

    Working on assignments

--- Working with Sinatra/book_viewer/book_viewer.rb
+++ Working with Sinatra/book_viewer/book_viewer.rb
@@ -8,7 +8,9 @@ end
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map { |par| '<p>' + par + '</p>'}.join
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
+    end.join
   end
 
   def each_chapter(&block)
@@ -25,10 +27,18 @@ helpers do
     return results unless query
 
     each_chapter do |number, name, contents|
-      results << {number: number, name: name} if contents.include?(query)
+      matches = {}
+      contents.split("\n\n").each_with_index do |paragraph, index|
+        matches[index] = paragraph if paragraph.include?(query)
+      end
+      results << {number: number, name: name, paragraphs: matches} if matches.any?
     end
 
     results
+  end
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
   end
 end
 
